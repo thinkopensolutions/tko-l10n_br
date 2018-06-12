605df3b82abff09a63963ae1335ef12bcf403c93
yk2kus <yogeshkushwaha4@gmail.com>
2018-06-12 12:05:45 +0000

    add cpf_cnpj in partner short form

---
+++ tko_br_base/__manifest__.py
@@ -0,0 +1,31 @@
+# © 2017 TKO <http://tko.tko-br.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Legal Names and CNPJ_CPF fields in Partner Short Form',
+    'summary': '',
+    'description': 'View of the Legal Name and CNPJ_CPF fields.',
+    'author': 'TKO',
+    'category': 'CRM',
+    'license': 'AGPL-3',
+    'website': 'http://tko.tko-br.com',
+    'version': '10.0.0.0.0',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': [
+                'br_base',
+    ],
+    'external_dependencies': {
+                                'python': [],
+                                'bin': [],
+                                },
+    'init_xml': [],
+    'update_xml': [],
+    'css': [],
+    'demo_xml': [],
+    'test': [],
+    'data': [
+             'views/res_partner_view.xml',
+    ],
+}
