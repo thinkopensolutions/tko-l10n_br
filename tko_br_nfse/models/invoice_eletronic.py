1a4d5ad2bd56ffded3829ac7bb6c33f5f4f3b967
yk2kus <yogeshkushwaha4@gmail.com>
2018-07-27 10:29:16 +0000

    update aliquota servico

--- tko_br_nfse/models/invoice_eletronic.py
+++ tko_br_nfse/models/invoice_eletronic.py
@@ -5,12 +5,18 @@ from openerp import fields, models, api
 STATE = {'edit': [('readonly', False)]}
 
 
+class BrAccountServiceType(models.Model):
+    _inherit = 'br_account.service.type'
+
+    issqn_percent = fields.Float(u'Aliquota serviço')
+
 class InvoiceEletronic(models.Model):
     _inherit = 'invoice.eletronic'
 
     nfse_url = fields.Char('URL', compute='view_nfse', store=False)
     valor_retencao_inss = fields.Monetary(
         string=u"Retenção INSS", readonly=True, states=STATE)
+    issqn_percent = fields.Float(u'Aliquota serviço', readonly=True, states=STATE)
 
     # FIX withholdings on NFSe emission
     @api.multi
@@ -20,9 +26,9 @@ class InvoiceEletronic(models.Model):
             res['lista_rps'][0].update({
                 'valor_pis': str("%.2f" % self.valor_retencao_pis),
                 'valor_cofins': str("%.2f" % self.valor_retencao_cofins),
-                'valor_csll': str("%.2f" % self.valor_retencao_csll),
                 'valor_inss': str("%.2f" % self.valor_retencao_inss),
                 'valor_ir': str("%.2f" % self.valor_retencao_irrf),
+                'aliquota_atividade': str("%.2f" % self.issqn_percent),
             })
         return res
 
